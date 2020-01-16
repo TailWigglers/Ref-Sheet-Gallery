e5b187a971c3162a883ef5f6d39d7ca62dc94385
Eric Auster <auster.eric@gmail.com>
2020-01-16 22:27:48 +0100

    Implement back and nsfw toggle

---
+++ js/reference.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("#toggle-event").change(function() {
+    if ($(this).prop("checked")) {
+      if (!confirm("You must be at least 18 years old to view this content!")) {
+        $("#toggle-event").bootstrapToggle("off");
+      }
+    }
+  });
+});
