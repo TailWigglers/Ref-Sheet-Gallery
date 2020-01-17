10eba55d34d1e91abbfe6775c2089b607c6a0a1d
Eric Auster <auster.eric@gmail.com>
2020-01-17 19:14:23 +0100

    Fix toggle for mobile

--- js/reference.js
+++ js/reference.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#toggle-event").change(function() {
+  $("#toggle-boi").change(function() {
     if ($(this).prop("checked")) {
       if (!confirm("You must be at least 18 years old to view this content!")) {
         $("#toggle-event").bootstrapToggle("off");
