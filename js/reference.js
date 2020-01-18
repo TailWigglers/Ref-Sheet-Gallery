20c29125dc72485209965cc12afc5d4ccc8bf1df
Eric Auster <auster.eric@gmail.com>
2020-01-18 12:38:14 +0100

    NSFW switch works

--- js/reference.js
+++ js/reference.js
@@ -1,9 +1,22 @@
 $(function() {
   $("#toggle-boi").change(function() {
+    var character = window.location.pathname.slice(1, -1);
     if ($(this).prop("checked")) {
       if (!confirm("You must be at least 18 years old to view this content!")) {
         $("#toggle-event").bootstrapToggle("off");
+      } else {
+        document.getElementById("character-bg").style.backgroundImage =
+          "url(../img/" + character.toLowerCase() + "_nsfw.png)";
+        document.getElementById("character-img").src = "../img/" + character.toLowerCase() + "_nsfw.png";
+        document.getElementById("character-dl").href = "../dwn/" + character + "NSFW.png";
+        document.getElementById("character-dl").download = character + "NSFW.png";
       }
+    } else {
+      document.getElementById("character-bg").style.backgroundImage =
+        "url(../img/" + character.toLowerCase() + "_sfw.png)";
+      document.getElementById("character-img").src = "../img/" + character.toLowerCase() + "_sfw.png";
+      document.getElementById("character-dl").href = "../dwn/" + character + "SFW.png";
+      document.getElementById("character-dl").download = character + "SFW.png";
     }
   });
 });
