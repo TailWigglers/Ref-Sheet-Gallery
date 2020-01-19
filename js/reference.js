88125ac43df4ffb2c06937bdc07b6bfe6c2b8ef1
Eric Auster <auster.eric@gmail.com>
2020-01-19 20:34:16 +0100

    Change to jpg for faster loading

--- js/reference.js
+++ js/reference.js
@@ -26,18 +26,18 @@ $(function() {
 function switchNSFW(character) {
   if (character === "Couple") {
     document.getElementById("seledrex-bg").style.backgroundImage =
-      "url(../img/" + seledrex.toLowerCase() + "_nsfw.png)";
-    document.getElementById("seledrex-img").src = "../img/" + seledrex.toLowerCase() + "_nsfw.png";
+      "url(../img/" + seledrex.toLowerCase() + "_nsfw.jpg)";
+    document.getElementById("seledrex-img").src = "../img/" + seledrex.toLowerCase() + "_nsfw.jpg";
     document.getElementById("seledrex-dl").href = "../dwn/" + seledrex + "NSFW.png";
     document.getElementById("seledrex-dl").download = seledrex + "NSFW.png";
-    document.getElementById("czero-bg").style.backgroundImage = "url(../img/" + czero.toLowerCase() + "_nsfw.png)";
-    document.getElementById("czero-img").src = "../img/" + czero.toLowerCase() + "_nsfw.png";
+    document.getElementById("czero-bg").style.backgroundImage = "url(../img/" + czero.toLowerCase() + "_nsfw.jpg)";
+    document.getElementById("czero-img").src = "../img/" + czero.toLowerCase() + "_nsfw.jpg";
     document.getElementById("czero-dl").href = "../dwn/" + czero + "NSFW.png";
     document.getElementById("czero-dl").download = czero + "NSFW.png";
   } else {
     document.getElementById("character-bg").style.backgroundImage =
-      "url(../img/" + character.toLowerCase() + "_nsfw.png)";
-    document.getElementById("character-img").src = "../img/" + character.toLowerCase() + "_nsfw.png";
+      "url(../img/" + character.toLowerCase() + "_nsfw.jpg)";
+    document.getElementById("character-img").src = "../img/" + character.toLowerCase() + "_nsfw.jpg";
     document.getElementById("character-dl").href = "../dwn/" + character + "NSFW.png";
     document.getElementById("character-dl").download = character + "NSFW.png";
   }
@@ -51,18 +51,18 @@ function switchNSFW(character) {
 
 function switchSFW(character) {
   if (character === "Couple") {
-    document.getElementById("seledrex-bg").style.backgroundImage = "url(../img/" + seledrex.toLowerCase() + "_sfw.png)";
-    document.getElementById("seledrex-img").src = "../img/" + seledrex.toLowerCase() + "_sfw.png";
+    document.getElementById("seledrex-bg").style.backgroundImage = "url(../img/" + seledrex.toLowerCase() + "_sfw.jpg)";
+    document.getElementById("seledrex-img").src = "../img/" + seledrex.toLowerCase() + "_sfw.jpg";
     document.getElementById("seledrex-dl").href = "../dwn/" + seledrex + "SFW.png";
     document.getElementById("seledrex-dl").download = seledrex + "SFW.png";
-    document.getElementById("czero-bg").style.backgroundImage = "url(../img/" + czero.toLowerCase() + "_sfw.png)";
-    document.getElementById("czero-img").src = "../img/" + czero.toLowerCase() + "_sfw.png";
+    document.getElementById("czero-bg").style.backgroundImage = "url(../img/" + czero.toLowerCase() + "_sfw.jpg)";
+    document.getElementById("czero-img").src = "../img/" + czero.toLowerCase() + "_sfw.jpg";
     document.getElementById("czero-dl").href = "../dwn/" + czero + "SFW.png";
     document.getElementById("czero-dl").download = czero + "SFW.png";
   } else {
     document.getElementById("character-bg").style.backgroundImage =
-      "url(../img/" + character.toLowerCase() + "_sfw.png)";
-    document.getElementById("character-img").src = "../img/" + character.toLowerCase() + "_sfw.png";
+      "url(../img/" + character.toLowerCase() + "_sfw.jpg)";
+    document.getElementById("character-img").src = "../img/" + character.toLowerCase() + "_sfw.jpg";
     document.getElementById("character-dl").href = "../dwn/" + character + "SFW.png";
     document.getElementById("character-dl").download = character + "SFW.png";
   }
